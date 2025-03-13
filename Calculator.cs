73616134076dcf1801874ed75d1db2f1ea71459a
HerodionKrisnalani <herodionkt199@students.amikom.ac.id>
2025-03-13 10:12:20 +0700

    Update fungsi Penambahan dan pengurangan

--- Calculator.cs
+++ Calculator.cs
@@ -3,15 +3,22 @@ class calculator
 	
 	static void main(string[] args)
 	{
+		int a = 10;
+		int b = 5;
+		Console.Writeline("Hasil Penambahan : {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.Writeline("Hasil Pengurangan : {0} + {1} = {2}", a, b, Pengurangan(a, b));
 		
-		
+		Console.Writeline("\nTekan Smbarang Key untuk keluar");
+		Console.Readkey();
 		
 	}
 	
 	static int Penambahan(int a, int b)
 	{
-		
-		return a+ b;
-		
+		return a + b;
+	}
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
